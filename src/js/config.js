4a4f1ba4e27ad61ef154fc0d967e6686302e4d18
Gabriela Melnik Carvalho <gabrielamcarvalho@gmail.com>
2021-05-31 11:52:31 +0100

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '2933f90c-91de-4109-8ef2-d639a15a6a25';
+export const MODAL_CLOSE_SEC = 2.5;
